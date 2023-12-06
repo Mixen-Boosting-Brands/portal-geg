e0880a1407a7662eb52a8e10e4272cb7d3694aa0
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-06 17:36:12 -0600

    Últimos avances

--- includes/header-interna.php
+++ includes/header-interna.php
@@ -5,7 +5,7 @@
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
             <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php the_title(); ?></h1>
         <?php endwhile; endif; ?>
-    <?php elseif ( 'procesos' == get_post_type() ): ?>
+    <?php elseif ( is_singular( 'procesos' ) ): ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
             <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php esc_html_e( 'Procesos de construcción', 'geg' ); ?></h1>
         <?php endwhile; endif; ?>
