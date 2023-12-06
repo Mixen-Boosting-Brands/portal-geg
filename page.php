a9ec7c762c8fb5998891821ccccbf20e5a1f1cf7
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-06 12:51:48 -0600

    Commentar título en vista de páginas

--- page.php
+++ page.php
@@ -6,11 +6,11 @@
 
 	<section class="py-60">
 		<div class="container">
-			<div class="row">
+			<!-- div class="row">
 				<div class="col text-center">
 					<h1 class="titulo-verde mb-4" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800" data-aos-once="true"><?php the_title(); ?></h1>
 				</div>
-			</div>
+			</div -->
 			<div class="row" data-aos="fade-right" data-aos-delay="600" data-aos-duration="800" data-aos-once="true">
 				<div class="col">
 				<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
