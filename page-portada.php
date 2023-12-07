a814d356b774345be76ea55b75843b8897aaf150
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-07 13:15:57 -0600

    Crear template Portada

---
+++ page-portada.php
@@ -0,0 +1,33 @@
+<?php 
+    /* Template Name: Portada */
+    get_header();
+    $currentlang = get_bloginfo('language');
+?>
+
+	<!-- Incluir header interna -->
+    <?php include get_template_directory() . '/includes/header-interna.php'; ?>
+    <!-- /Incluir header interna -->
+
+	<section class="py-60">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+                    tuut
+					<h1 class="titulo-verde mb-4" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800" data-aos-once="true"><?php the_title(); ?></h1>
+				</div>
+			</div>
+			<div class="row" data-aos="fade-right" data-aos-delay="600" data-aos-duration="800" data-aos-once="true">
+				<div class="col">
+				<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+					<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
+				<?php endwhile; ?>
+				<?php else : ?>
+					<p><?php esc_html_e( 'Lo sentimos, no hay nada que mostrar aquí.', 'geg' ); ?></p>
+				<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
