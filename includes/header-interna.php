26888d90ad7041d4b94964a5dc3159f9b60d47cb
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-08 13:53:10 -0600

    Ajuste en header

--- includes/header-interna.php
+++ includes/header-interna.php
@@ -1,13 +1,13 @@
 <section class="banner-superior" data-parallax="scroll" data-image-src="<?php if( !is_search() ): ?><?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?><?php the_sub_field('imagen'); ?><?php endwhile; endif; ?><?php else: ?><?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/404/header-404.png<?php endif; ?>">
     <div class="overlay">
         <div class="caption">
-    <?php if( !is_search() ): ?>
-        <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
-            <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php the_title(); ?></h1>
-        <?php endwhile; endif; ?>
-    <?php elseif ( is_singular( 'procesos' ) ): ?>
+    <?php if ( is_singular( 'procesos' ) ): ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
             <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php esc_html_e( 'Procesos de construcción', 'geg' ); ?></h1>
+        <?php endwhile; endif; ?>
+    <?php elseif ( !is_search() ): ?>
+        <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
+            <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php the_title(); ?></h1>
         <?php endwhile; endif; ?>
     <?php endif; ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
