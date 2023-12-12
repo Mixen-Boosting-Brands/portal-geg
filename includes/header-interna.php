eddc6617ca7508300b0b8d3cd927b487e15a5e1f
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-12 11:51:54 -0600

    Actualizar título

--- includes/header-interna.php
+++ includes/header-interna.php
@@ -7,6 +7,10 @@
         <?php endwhile; endif; ?>
     <?php elseif ( !is_search() ): ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
+            <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php esc_html_e( 'Oferta de valor', 'geg' ); ?><</h1>
+        <?php endwhile; endif; ?>
+    <?php elseif ( !is_search() ): ?>
+        <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
             <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php the_title(); ?></h1>
         <?php endwhile; endif; ?>
     <?php endif; ?>
