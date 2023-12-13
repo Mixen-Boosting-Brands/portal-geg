823d122f8ffe8991ec2b9af3af5d1e947dfd5b7b
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-13 17:10:34 -0600

    Modificar código que triggerea modal para sanitizar atributo data-bs-cv

--- assets/src/modals.js
+++ assets/src/modals.js
@@ -5,6 +5,11 @@ function sanitizeHTML(html) {
     return DOMPurify.sanitize(html);
 }
 
+// Function to replace double quotes with HTML entities
+function replaceDoubleQuotes(str) {
+    return str.replace(/"/g, '&quot;');
+}
+
 // Event listener for modal trigger button
 document.querySelectorAll('[data-bs-toggle="modal"]').forEach(function (button) {
     button.addEventListener('click', function () {
@@ -15,6 +20,9 @@ document.querySelectorAll('[data-bs-toggle="modal"]').forEach(function (button) 
         var title = button.getAttribute('data-bs-titulo');
         var thumbnailUrl = button.getAttribute('data-bs-thumbnail-url');
         var cv = button.getAttribute('data-bs-cv');
+
+        // Replace double quotes with HTML entities
+        cv = replaceDoubleQuotes(cv);
 
         // Sanitize and set modal content
         modal.querySelector('.modal-title').innerText = title;
