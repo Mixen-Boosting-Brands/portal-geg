6257e773e0f2932e6507a397c3cf687b7053fadf
Luis Carlos Pando <hey@luiscarlospando.com>
2024-04-17 12:43:36 -0600

    Corregir chequeo de página para mostrar header correcto

--- includes/header-interna.php
+++ includes/header-interna.php
@@ -7,7 +7,7 @@
         <?php endwhile; endif; ?>
     <?php elseif ( is_page( 'oferta-de-valor' ) ): ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
-            <h1 data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php esc_html_e( 'Oferta de valor', 'geg' ); ?><</h1>
+            <h1 testeo data-aos="fade-right" data-aos-once="true" <?php if( get_sub_field('color_del_titulo') ): ?>style="color: #<?php the_sub_field('color_del_titulo'); ?>;"<?php endif; ?>><?php esc_html_e( 'Oferta de valor', 'geg' ); ?><</h1>
         <?php endwhile; endif; ?>
     <?php elseif ( !is_search() ): ?>
         <?php if( have_rows('header') ): while( have_rows('header') ): the_row(); ?>
